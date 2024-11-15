3b414d8ef3b07acd08cd420c489e99911183a479
Oleksii Donchenko <alexeyip68@gmail.com>
2024-11-15 23:33:07 +0200

    Game logic. Highlighting of available cells for moves.

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,12 +2,19 @@ import { FC } from 'react';
 import { Cell } from '../modules/Cell';
 
 interface CellProps {
-  cell: Cell
+  cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <div className={['cell', cell.color,
+      selected ? 'selected' : '',
+      cell.available && cell.figure ? 'attacked' : ''].join(' ')}
+      onClick={() => click(cell)}
+    >
+      {cell.available && !cell.figure && <div className={'available'} />}
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
