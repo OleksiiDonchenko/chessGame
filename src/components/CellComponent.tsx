c8d8c30a1279561e9744b58720532baeafbee4c5
Oleksii Donchenko <alexeyip68@gmail.com>
2025-01-06 18:16:53 +0200

    Added numbering form "a" to "f" at the bottom and from "1" to "8" on the right

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -10,6 +10,9 @@ interface CellProps {
 }
 
 const CellComponent: FC<CellProps> = ({ cell, selected, click, y, x }) => {
+  const numbers = [8, 7, 6, 5, 4, 3, 2, 1];
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
   return (
     <div className={['cell', cell.color,
       selected ? 'selected' : '',
@@ -17,7 +20,16 @@ const CellComponent: FC<CellProps> = ({ cell, selected, click, y, x }) => {
       cell.isKingInCheck ? 'check' : ''].join(' ')}
       onClick={() => click(cell)}
     >
-      <span className='index'>{x};{y}</span>
+      {numbers.map((number, i) => {
+        if (x === 7 && y === i) {
+          return <span key={i} className={['number', cell.color === 'black' ? '_white' : '_black'].join(' ')}>{number}</span>
+        }
+      })}
+      {letters.map((letter, i) => {
+        if (y === 7 && x === i) {
+          return <span key={i} className={['letter', cell.color === 'black' ? '_white' : '_black'].join(' ')}>{letter}</span>
+        }
+      })}
       {cell.available && !cell.figure && <div className={'available'} />}
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
