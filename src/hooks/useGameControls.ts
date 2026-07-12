49a0a6ee437424e6302c41c5edfdc148e17e802d
Oleksii Donchenko <alexeyip68@gmail.com>
2026-07-12 15:35:18 +0300

    Refactor: improve game controls hook

--- src/hooks/useGameControls.ts
+++ src/hooks/useGameControls.ts
@@ -2,6 +2,7 @@ import { Colors } from "../modules/Colors";
 import { useChessContext } from "../context/ChessContext";
 import { Player } from "../modules/Player.ts";
 import { Square } from "../modules/board/Square.ts";
+import { useCallback } from "react";
 
 interface UseGameControlsParams {
   setCurrentPlayer: (p: Player | null) => void;
@@ -14,41 +15,61 @@ interface UseGameControlsParams {
 export function useGameControls({ setCurrentPlayer, setSelectedSquare, restart, resetTimers, whitePlayer }: UseGameControlsParams) {
   const { board, setGameIsOn, setGameWasStarted, setIsAnalysis } = useChessContext();
 
-  function handleRestart() {
+  const handleRestart = useCallback(() => {
     setGameIsOn(false);
     setGameWasStarted(false);
+    setIsAnalysis(false);
     setCurrentPlayer(null);
     setSelectedSquare(null);
+
     resetTimers();
     restart();
-  }
+  }, [setGameIsOn,
+    setGameWasStarted,
+    setIsAnalysis,
+    setCurrentPlayer,
+    setSelectedSquare,
+    resetTimers,
+    restart]);
 
-  function handleStartGame() {
+  const handleStartGame = useCallback(() => {
     setIsAnalysis(false);
     setGameIsOn(true);
     setGameWasStarted(true);
     setCurrentPlayer(whitePlayer);
-  }
+  }, [setIsAnalysis,
+    setGameIsOn,
+    setGameWasStarted,
+    setCurrentPlayer,
+    whitePlayer]);
 
-  function handleAnalysis() {
+  const handleAnalysis = useCallback(() => {
     setIsAnalysis(true);
     setGameIsOn(false);
     setGameWasStarted(true);
     setCurrentPlayer(whitePlayer);
-  }
+  }, [setIsAnalysis,
+    setGameIsOn,
+    setGameWasStarted,
+    setCurrentPlayer,
+    whitePlayer]);
 
-  function handleStopGame(color?: Colors) {
+  const handleStopGame = useCallback((color?: Colors) => {
     if (color)
       board.handleResign(color);
     setGameIsOn(false);
     setCurrentPlayer(null);
     setSelectedSquare(null);
-  }
+  }, [board,
+    setGameIsOn,
+    setCurrentPlayer,
+    setSelectedSquare]);
 
-  function handleDraw(color: Colors) {
+  const handleDraw = useCallback((color: Colors) => {
     board.handleDraw(color);
     handleStopGame();
-  }
+  }, [board,
+    handleStopGame]);
 
   return {
     handleRestart, handleStartGame, handleAnalysis, handleStopGame, handleDraw
