16a69c20b412deca32c668adf8b3011af17cdfe4
Oleksii Donchenko <alexeyip68@gmail.com>
2026-07-21 17:53:04 +0300

    Refactor: change CSS class names

--- src/components/board/ChessBoard.tsx
+++ src/components/board/ChessBoard.tsx
@@ -33,90 +33,88 @@ function ChessBoard() {
 
   return (
     <>
-      <div className='wrapper'>
-        <div className='wrapper-board'>
-          <GameControls handleRestart={handleRestart}
-            handleStartGame={handleStartGame}
-            handleAnalysis={handleAnalysis}
-            handleStopGame={handleStopGame}
-            handleDraw={handleDraw}
-            gameIsOn={gameIsOn}
-            gameWasStarted={gameWasStarted}
-            currentPlayer={currentPlayer} />
-          <div className='capturedPiecesAndTime'>
-            <CapturedPieces
-              color='white'
-              pieces={board.lostWhitePieces}
-              whoLeads={whoLeads}
-              setWholeads={setWholeads}
-              whitePoints={whitePoints}
-              setWhitePoints={setWhitePoints}
-              blackPoints={blackPoints}
-              setBlackPoints={setBlackPoints} />
-            <div className={['time', 'blackTime', currentPlayer === blackPlayer && !isAnalysis ? 'goes' : ''].join(' ')}>
-              <Clock fill='white' />
-              <span>
-                {blackFormattedTime}
-              </span>
-            </div>
-          </div>
-          <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd} onDragCancel={handleDragCancel} modifiers={[restrictToWindowEdges]}>
-            <div ref={boardRef} className={['board', promotionSquare ? 'eclipse' : ''].join(' ')} onClick={() => clickOnTheBoard()}>
-              {promotionSquare && (
-                <PromotionModal onSelect={handlePromotion} x={promotionSquare.x} color={promotionSquare.piece?.color}
-                  square={promotionSquare} />
-              )}
-              {board.squares.map((row, y) => <React.Fragment key={y}>
-                {row.map((square) => <DroppableSquare
-                  key={square.id}
-                  id={`${square.x}-${square.y}`}
-                  color={square.color}
-                  selected={square.x === selectedSquare?.x && square.y === selectedSquare?.y}
-                  isAvailable={square.available}
-                  isKingInCheck={square.isKingInCheck}
-                  isCheckmate={square.isCheckmate}
-                  resign={square.resign}
-                  losingByTime={square.losingByTime}
-                  isVictory={square.isVictory}
-                  isStalemate={square.isStalemate}
-                  isDraw={square.isDraw}
-                  handleStopGame={handleStopGame}
-                  square={square}
-                  mouseDown={mouseDown}
-                  coordinates={{ x: square.x, y: square.y }}
-                >
-                  {square.piece?.logo && (
-                    <DraggablePiece
-                      id={`${square.x}-${square.y}`}
-                      src={square.piece.logo} />
-                  )}
-                </DroppableSquare>
-                )}
-              </React.Fragment>
-              )}
-            </div>
-          </DndContext>
-          <div className='capturedPiecesAndTime'>
-            <CapturedPieces
-              color='black'
-              pieces={board.lostBlackPieces}
-              whoLeads={whoLeads}
-              setWholeads={setWholeads}
-              whitePoints={whitePoints}
-              setWhitePoints={setWhitePoints}
-              blackPoints={blackPoints}
-              setBlackPoints={setBlackPoints} />
-            <div className={['time', 'whiteTime', currentPlayer === whitePlayer && !isAnalysis ? 'goes' : ''].join(' ')}>
-              <Clock fill='black' />
-              <span>
-                {whiteFormattedTime}
-              </span>
-            </div>
+      <div className='chess-game'>
+        <GameControls handleRestart={handleRestart}
+          handleStartGame={handleStartGame}
+          handleAnalysis={handleAnalysis}
+          handleStopGame={handleStopGame}
+          handleDraw={handleDraw}
+          gameIsOn={gameIsOn}
+          gameWasStarted={gameWasStarted}
+          currentPlayer={currentPlayer} />
+        <div className='capturedPiecesAndClock'>
+          <CapturedPieces
+            color='white'
+            pieces={board.lostWhitePieces}
+            whoLeads={whoLeads}
+            setWholeads={setWholeads}
+            whitePoints={whitePoints}
+            setWhitePoints={setWhitePoints}
+            blackPoints={blackPoints}
+            setBlackPoints={setBlackPoints} />
+          <div className={['time', 'blackTime', currentPlayer === blackPlayer && !isAnalysis ? 'goes' : ''].join(' ')}>
+            <Clock fill='white' />
+            <span>
+              {blackFormattedTime}
+            </span>
           </div>
         </div>
-        <GameSidebar boardRef={boardRef} clickOnBoard={clickOnBoard} setClickOnBoard={setClickOnBoard}
-          swapPlayer={swapPlayer} isAnalysis={isAnalysis} setSelectedSquare={setSelectedSquare} currentPlayer={currentPlayer} />
+        <DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd} onDragCancel={handleDragCancel} modifiers={[restrictToWindowEdges]}>
+          <div ref={boardRef} className={['board', promotionSquare ? 'eclipse' : ''].join(' ')} onClick={() => clickOnTheBoard()}>
+            {promotionSquare && (
+              <PromotionModal onSelect={handlePromotion} x={promotionSquare.x} color={promotionSquare.piece?.color}
+                square={promotionSquare} />
+            )}
+            {board.squares.map((row, y) => <React.Fragment key={y}>
+              {row.map((square) => <DroppableSquare
+                key={square.id}
+                id={`${square.x}-${square.y}`}
+                color={square.color}
+                selected={square.x === selectedSquare?.x && square.y === selectedSquare?.y}
+                isAvailable={square.available}
+                isKingInCheck={square.isKingInCheck}
+                isCheckmate={square.isCheckmate}
+                resign={square.resign}
+                losingByTime={square.losingByTime}
+                isVictory={square.isVictory}
+                isStalemate={square.isStalemate}
+                isDraw={square.isDraw}
+                handleStopGame={handleStopGame}
+                square={square}
+                mouseDown={mouseDown}
+                coordinates={{ x: square.x, y: square.y }}
+              >
+                {square.piece?.logo && (
+                  <DraggablePiece
+                    id={`${square.x}-${square.y}`}
+                    src={square.piece.logo} />
+                )}
+              </DroppableSquare>
+              )}
+            </React.Fragment>
+            )}
+          </div>
+        </DndContext>
+        <div className='capturedPiecesAndClock'>
+          <CapturedPieces
+            color='black'
+            pieces={board.lostBlackPieces}
+            whoLeads={whoLeads}
+            setWholeads={setWholeads}
+            whitePoints={whitePoints}
+            setWhitePoints={setWhitePoints}
+            blackPoints={blackPoints}
+            setBlackPoints={setBlackPoints} />
+          <div className={['time', 'whiteTime', currentPlayer === whitePlayer && !isAnalysis ? 'goes' : ''].join(' ')}>
+            <Clock fill='black' />
+            <span>
+              {whiteFormattedTime}
+            </span>
+          </div>
+        </div>
       </div>
+      <GameSidebar boardRef={boardRef} clickOnBoard={clickOnBoard} setClickOnBoard={setClickOnBoard}
+        swapPlayer={swapPlayer} isAnalysis={isAnalysis} setSelectedSquare={setSelectedSquare} currentPlayer={currentPlayer} />
     </>
   );
 }
